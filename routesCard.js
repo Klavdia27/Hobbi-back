610f9ea85a388c69286bfcc43d52ed1a064b3ab9
Fuad <fuad.fuad.1984@list.ru>
2023-01-09 18:49:36 +0400

    admin

--- routesCard.js
+++ routesCard.js
@@ -7,13 +7,9 @@ const router = new Router()
 router.get('/card/filter', (req, res) => {
     const cards = mongoose.model('hobby', cardSchema)
 
-    console.log('sdsd')
-
-    // cards.findById(req.params.id, null, {},function (err, resu) {
-    //     //     res.status(200).json(resu)
-    //     // });
-
-    res.status(200).json(req.query)
+    cards.find({section: req.query.section, subsection: req.query.subsection}, null, {},function (err, resu) {
+        res.status(200).json(resu)
+    });
 })
 
 
@@ -37,20 +33,9 @@ router.get('/card/:id', (req, res) => {
 router.post('/create/card', (req, res) => {
     const card = mongoose.model('hobby', cardSchema)
 
-    const newCard = new card({
-        "name": "Лошадь, зебра, единорог",
-        "section": "Вязание для дома",
-        "subsection": "Игрушки",
-        "description": "Вязаные лошадь, зебра, единорог по одной схеме вязания.",
-        "author": "",
-        "material": "",
-        "conventions": "",
-        "link": "",
-        "date": "25/11/2022",
-        "countcomment": "3",
-        "watch": "8",
-        "image": "https://1.bp.blogspot.com/-cNUkA1HkRK4/VsdC8JluGZI/AAAAAAAAE4I/zEBfKWjnUjM/s1600/3%25D0%25B8%25D0%25B3%25D1%2580%25D1%2583%25D1%2588%25D0%25BA%25D0%25B8.PNG",
-    })
+    console.log(req.body.data)
+
+    const newCard = new card(req.body.data)
 
     newCard.save()
 })
